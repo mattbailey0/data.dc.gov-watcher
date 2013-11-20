afe0331f90ec86482a49e17ccb728679f9f49649
mattbailey0 <felbot@gmail.com>
2013-11-20 12:52:22 -0500

    Update scaper.rb
    add all columns from main table on data.dc.gov

--- scaper.rb
+++ scaper.rb
@@ -5,26 +5,25 @@ require 'open-uri'
 require 'nokogiri'
 
 doc = Nokogiri::HTML(open("http://data.dc.gov"))
-#ScraperWiki::sqliteexecute("drop table if exists swdata")
+
+columns = [ 'title', 'metadata', 'source', 'text-csv', 'atom', 'kml-esri', 'gmap', 'custom_dl' ]
+uid = "title_href_0"
 
 doc.search("table#ctl00_cphMain_mainTable tr").each do |row|
     cells = row.search 'td'
     if cells.count > 0 && cells[0].attr("class") != "minor"
-      data = {
-        'title' => cells[0].text,
-        'metadata_href' => cells[0].xpath("a").attr("href"),
-        'source' => cells[1].text,
-        'source_href' => cells[1].xpath("a").attr("href")
-        
-        #'text_csv_href' => cells[3].xpath("a").attr("href"),
-        #'atom_href' => cells[4].xpath("a").attr("href"),
-        #'kml_esri_href' => cells[5].xpath("a").attr("href"),
-        #'gmap_href' => cells[6].xpath("a").attr("href"),
-        #'cust_download_href' => cells[7].xpath("a").attr("href")
-        }
-      if cells[2].xpath("a")[0] 
-        data['xml_href'] = cells[2].xpath("a")[0].attr("href")
+      data = Hash.new
+
+      for i in 0..columns.count-1      
+        key = columns[i] + "_txt"
+        data[key] = cells[i].text
+
+        for ii in 0..cells[i].xpath("a").count-1
+          key = columns[i] + "_href_" + ii.to_s
+          data[key] = cells[i].xpath("a")[ii].attr("href")
+        end
       end
-      ScraperWiki.save_sqlite(['title'], data)  
+      
+      ScraperWiki.save_sqlite([uid], data)  
     end
 end
