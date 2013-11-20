057df50ad157a3e147a96b3ac74392fa2cd0b3eb
mattbailey0 <felbot@gmail.com>
2013-11-19 19:12:51 -0500

    Update scaper.rb

--- scaper.rb
+++ scaper.rb
@@ -5,14 +5,26 @@ require 'open-uri'
 require 'nokogiri'
 
 doc = Nokogiri::HTML(open("http://data.dc.gov"))
-ScraperWiki::sqliteexecute("drop table if exists swdata")
+#ScraperWiki::sqliteexecute("drop table if exists swdata")
 
 doc.search("table#ctl00_cphMain_mainTable tr").each do |row|
     cells = row.search 'td'
     if cells.count > 0 && cells[0].attr("class") != "minor"
       data = {
-        'title' => cells[0].text
+        'title' => cells[0].text,
+        'metadata_href' => cells[0].xpath("a").attr("href"),
+        'source' => cells[1].text,
+        'source_href' => cells[1].xpath("a").attr("href")
+        
+        #'text_csv_href' => cells[3].xpath("a").attr("href"),
+        #'atom_href' => cells[4].xpath("a").attr("href"),
+        #'kml_esri_href' => cells[5].xpath("a").attr("href"),
+        #'gmap_href' => cells[6].xpath("a").attr("href"),
+        #'cust_download_href' => cells[7].xpath("a").attr("href")
         }
+      if cells[2].xpath("a")[0] 
+        data['xml_href'] = cells[2].xpath("a")[0].attr("href")
+      end
       ScraperWiki.save_sqlite(['title'], data)  
     end
 end
